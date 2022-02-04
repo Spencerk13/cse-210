57af0b226d65c74b830c827542418cb760fc4f56
Samuel Casellas <samcomon@gmail.com>
2022-02-03 17:54:12 -0700

    Finished parachute class

--- jump_game/parachute.py
+++ jump_game/parachute.py
@@ -1,3 +1,30 @@
 class Parachute:
     def __init__(self):
-        self._chute = '___'
+        self._parachute = [
+            "     ___     ",
+            "    /___\    ",
+            "    \   /    ",
+            "     \ /     ",
+            "      O      ",
+            "     /|\     ",
+            "     / \     ",
+            "             ",
+            "^^^^^^^^^^^^^"
+        ]
+    
+    def disintegrate(self):
+        self._parachute.remove(self._parachute[0])
+
+    def show_parachute(self):
+        parachute_text = ""
+        for unit in self._parachute:
+            parachute_text += unit + "\n"
+        return parachute_text
+
+"""
+parachute = Parachute()
+
+for _ in range(4):
+    print(parachute.show_parachute())
+    parachute.disintegrate()
+"""
