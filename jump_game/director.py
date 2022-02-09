c2fa8415124fa5f8af07a5787765dbdabfcc0d12
James Chan <jhchan99@gmail.com>
2022-02-09 14:58:31 -0700

    named new stuff in constructor

--- jump_game/director.py
+++ jump_game/director.py
@@ -4,12 +4,17 @@ from jump_game.puzzle import Puzzle
 class Director:
     def __init__(self):
         self._is_playing = True
+        self.parachute = Parachute()
+        self.guess = Guess()
+        self.puzzle = Puzzle()
+
     def start_game(self):
         while self._is_playing:
             self.do_inputs()
             self.do_outputs()
+
     def do_inputs(self):
-        guess = input("Guess a letter [A-Z]: ")
+        self.guess = input("Guess a letter [A-Z]: ")
 
     def do_outputs(self):
         print("done")
