297bac64ed0609a3487b39fa7c6a0783dae9ac59
Samuel Casellas <samcomon@gmail.com>
2022-02-10 08:14:17 -0700

    Little update on the read me file

--- jump_game/__main__.py
+++ jump_game/__main__.py
@@ -1,3 +1,8 @@
-from jump_game.director import Director
-director = Director()
-director.start_game()
+def main():
+
+    from jump_game.director import Director
+    director = Director()
+    director.start_game()
+
+if __name__ == "__main__":
+    main()
