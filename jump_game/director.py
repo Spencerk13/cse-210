a021702286aac3e0e5f9571f06330d9c8b0a975c
James Chan <jhchan99@gmail.com>
2022-02-11 14:11:11 -0700

    i dont knwo

--- jump_game/director.py
+++ jump_game/director.py
@@ -1,16 +1,31 @@
-from jump_game.parachute import Parachute
-from jump_game.terminal_service import Terminal_Service
-from jump_game.puzzle import Puzzle
+from parachute import Parachute
+from terminal_service import Terminal_Service
+from puzzle import Puzzle
+
 class Director:
+
     def __init__(self):
         self._is_playing = True
-        self._guess =''
+        self._guess = ''
+        self.parachute = Parachute()
+        self.terminal_service = Terminal_Service()
+        self.puzzle = Puzzle()
+
     def start_game(self):
         while self._is_playing:
+            self.parachute.print_image()
             self.do_inputs()
             self.do_outputs()
+            self._is_playing = self.puzzle.
+
+
     def do_inputs(self):
-        guess = input("Guess a letter [A-Z]: ")
-        self._guess=guess
+        self._guess = input("Guess a letter [A-Z]: ")
+        self.puzzle.something(self._guess)
+
     def do_outputs(self):
+        print(f"Guessed: {self._guess}")
+        if not self.puzzle._correct :
+            print('Youre guess was incorrect!')
+            self.parachute.update_image()
         print("done")
--- jump_game/terminal_service.py
+++ jump_game/terminal_service.py
@@ -1,10 +1,10 @@
 class TerminalService:
-     
+
     def _read_text(self, prompt):
         return input(prompt)
 
     def _read_guess(self, prompt):
         return (input(prompt))
-        
+           
     def _write_text(self, text):
         print(text)
